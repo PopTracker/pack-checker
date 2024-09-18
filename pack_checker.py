4bd571d135dec9bb08ff77b7897e04054cc12003
black-sliver <59490463+black-sliver@users.noreply.github.com>
2024-09-18 12:44:51 +0200

    bump version to 1.3.0

--- pack_checker.py
+++ pack_checker.py
@@ -35,7 +35,7 @@ from jsonc import parse as parse_jsonc, ParserError as JsonParserError
 from ziputil import ZipPath
 
 
-__version_info__ = (1, 2, 0)
+__version_info__ = (1, 3, 0)
 __version__ = ".".join(map(str, __version_info__))
 
 PY = sys.version_info
