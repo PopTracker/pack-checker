fd999ea75caffb92fee5f554bbb66fa4e72356c2
black-sliver <59490463+black-sliver@users.noreply.github.com>
2026-04-12 13:03:53 +0200

    Reexport Checker as pack_checker.Checker

--- pack_checker/__init__.py
+++ pack_checker/__init__.py
@@ -1,2 +1,7 @@
 __version_info__ = (1, 6, 0)
 __version__ = ".".join(map(str, __version_info__))
+
+# public API for use as lib
+from .checker import Checker
+
+__all__ = ["__version__", "__version_info__", "Checker"]
